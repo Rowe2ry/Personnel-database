f720955299511bdd93d9df16b8cef228cb1adc58
Chris Rowe <rowe2ry@yahoo.com>
2022-04-24 15:09:19 -0400

    Writing utility functions for inquirer menu prompts.

--- utilities/mysql.js
+++ utilities/mysql.js
@@ -50,5 +50,55 @@ class SQL {
     // CHANGE role_id to whatever
     changeRole(empId, role_id) {
         return this.mysql.promise().query(`UPDATE employee SET role_id = "${role_id}" WHERE id = ${empId};`);
-    }
-}
+    };
+
+    // function that gets the current list of departments by name and returns and array
+    deptList() {
+        this.mysql.promise().query('SELECT department.dept_name FROM department;', function(err, results) {
+            const departments = []; // new empty array
+            results.forEach(result => departments.push(result.dept_name)); // go thru array of objects and just pull out the department name values
+            return departments; // return new array of department names
+        });
+    };
+
+    // Get department id number by name
+    deptNameToId(str) {
+        const deptIdNum = this.mysql.promise().query(`SELECT id FROM department WHERE dept_name = '${str}';`);
+        return deptIdNum[0][0];
+    };
+
+    // function that gets the current list of roles by name and puts them in an array
+    roleList() {
+        this.mysql.promise().query('SELECT role.title FROM role;', function(err, results) {
+            const roles = []; // new empty array
+            results.forEach(result => departments.push(result.title)); // go thru array of objects and just pull out the role title values
+            return roles; // return new array of department names
+        });
+    };
+
+    // Get role id number by name
+    roleNameToId(str) {
+        const roleIdNum = this.mysql.promise().query(`SELECT id FROM role WHERE title = '${str}';`);
+        return roleIdNum[0][0];
+    };
+
+    // function that gets the current list of employees by name and puts them in an array
+    empList() {
+        this.mysql.promise().query('SELECT employee.first_name, employee.last_name FROM employee;', function(err, results) {
+            const employees = []; // new empty array
+            results.forEach(result => departments.push(result.first_name.concat(` ${result.last_name}`))); // go thru array of objects and just pull out the department name values
+            return employees; // return new array of department names
+        });
+    };
+
+    // Get employee id number by first and last name
+    empNameToId(str) {
+        const fullName = str.split(' ');
+        const first = fullName[0];
+        const last = fullName[1];
+        const employeeIdNum = this.mysql.promise().query(`SELECT id FROM employee WHERE first_name = '${first}' AND last_name = '${last}';`);
+        return employeeIdNum[0][0];
+    };
+};
+
+module.exports = new SQL(db);
